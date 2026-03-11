7b9f07ff3921c774a486314a9e06abbf7d8fc6e8
LucasMcDo-crypto <Lucas.MSN@eduge.ch>
2026-03-11 11:20:13 +0100

    Create main server program for multiplayer game
    Initial implementation of the main server program that handles game sessions.

---
+++ server_main.py
@@ -0,0 +1,8 @@
+"""Programme principal qui créer une partie complète avec plusieurs joueurs
+
+"""
+
+1. créer GameServer
+2. écouter connexions (stocke les joueurs)
+3. créer partie quand joueurs prêts
+4. boucle infinie
