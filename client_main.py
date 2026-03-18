277a52751fa9f174d325954f920af85093582bd4
LucasMcDo-crypto <Lucas.MSN@eduge.ch>
2026-03-18 11:08:29 +0100

    Implement affichage function for game state display
    Add affichage function to display game state in UI

--- client_main.py
+++ client_main.py
@@ -7,3 +7,27 @@ import ui
 2. créer controller
 3. connecter serveur
 4. lancer UI loop
+
+def affichage(game_state):
+    """Affiche la UI avec un GameState."""
+
+    root = Tk.Tk()
+    root.title("MVC - UNO TEST")
+    root.state("zoomed")
+
+    root.rowconfigure(0, weight=1)
+    root.columnconfigure(0, weight=1)
+
+    controle = Controle(root)
+
+    # ---------------------------
+    # GAME STATE 
+    # ---------------------------
+  
+    # Injecte l'état 
+    controle.recevoir_game_state(game_state)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    _test()
