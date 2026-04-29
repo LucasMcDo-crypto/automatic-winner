f4a545b403bb4cbb2a8bf4dd981faa84b4746233
LucasMcDo-crypto <Lucas.MSN@eduge.ch>
2026-04-29 10:16:50 +0200

    Refactor player card representation in fake_state.py

--- fake_state.py
+++ fake_state.py
@@ -7,22 +7,60 @@ fake_state = {
     "direction": 1,
     "current_player": "Lucas",
     "players": [
-        {"name": "Robert", "cards": 8},
-        {"name": "Joris", "cards": 2},
-        {"name": "Jean-Louis", "cards": 15},
-        {"name": "Chien", "cards": 7},
-        {"name": "Kevin", "cards": 10},
-        {"name": "Alexis", "cards": 0},
-        {"name": "Lucas", "cards": 4},
-        {"name": "JSP", "cards": 1},
-    ],
-    "your_hand": [
-        "Green-1",
-        "Green-1",
-        "Green-1",
-        "Green-1",
-        "Green-1",
-        "Green-1"
+        {
+            "name": "Robert",
+            "cards": [
+                "Red-3", "Blue-5", "Yellow-2", "Green-7",
+                "Wild", "Red-Skip", "Blue-1", "Yellow-9"
+            ]
+        },
+        {
+            "name": "Joris",
+            "cards": [
+                "Green-4", "Red-Draw2"
+            ]
+        },
+        {
+            "name": "Jean-Louis",
+            "cards": [
+                "Blue-0", "Blue-2", "Blue-7", "Green-5", "Green-9",
+                "Yellow-1", "Yellow-4", "Yellow-6", "Yellow-Reverse",
+                "Red-1", "Red-5", "Red-8", "Wild+4", "Green-Skip", "Blue-Reverse"
+            ]
+        },
+        {
+            "name": "Chien",
+            "cards": [
+                "Green-2", "Green-3", "Blue-9",
+                "Yellow-7", "Red-6", "Wild", "Blue-Skip"
+            ]
+        },
+        {
+            "name": "Kevin",
+            "cards": [
+                "Red-0", "Red-2", "Red-4", "Blue-3", "Blue-8",
+                "Green-6", "Yellow-5", "Yellow-8", "Wild+4", "Green-Reverse"
+            ]
+        },
+        {
+            "name": "Alexis",
+            "cards": []
+        },
+        {
+            "name": "Lucas",
+            "cards": [
+                "Green-1",
+                "Green-1",
+                "Green-1",
+                "Green-1"
+            ]
+        },
+        {
+            "name": "JSP",
+            "cards": [
+                "Red-9"
+            ]
+        },
     ],
     "draw_stack": 0,
     "winner": None
