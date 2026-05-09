803b4fefed34c9f7c06a6baefeb8958c01f04dbf
LucasMcDo-crypto <Lucas.MSN@eduge.ch>
2026-05-09 13:50:52 +0200

    Update discard value and player card lists

--- fake_state.py
+++ fake_state.py
@@ -3,9 +3,16 @@
 Fait par Lucas"""
 
 fake_state = {
-    "discard": "Green-1",
+    "discard": "v1",
+
     "direction": 1,
+
     "current_player": "Lucas",
+
+    "etat_main": [
+        "normal", "normal", "disabled", "normal"
+    ],
+
     "players": [
         {
             "name": "Robert",
@@ -13,18 +20,22 @@ fake_state = {
                 "r3", "b5", "j2", "v7", "r0", "b1", "j9"
             ]
         },
+
         {
             "name": "Joris",
             "cards": [
                 "v4", "r2"
             ]
         },
+
         {
             "name": "Jean-Louis",
             "cards": [
                 "b0", "b2", "b7", "v5", "v9",
-                "j1", "j4", "j6"]
+                "j1", "j4", "j6"
+            ]
         },
+
         {
             "name": "Chien",
             "cards": [
@@ -32,28 +43,37 @@ fake_state = {
                 "Yellow-7", "Red-6", "Wild", "Blue-Skip"
             ]
         },
+
         {
             "name": "Kevin",
             "cards": [
-                "r0", "r2", "r4"]
+                "r0", "r2", "r4"
+            ]
         },
+
         {
             "name": "Alexis",
-            "cards": ["j3"]
+            "cards": [
+                "j3"
+            ]
         },
+
         {
             "name": "Lucas",
             "cards": [
                 "j1", "v1", "v1", "v1"
             ]
         },
+
         {
             "name": "JSP",
             "cards": [
                 "r9"
             ]
-        },
+        }
     ],
+
     "draw_stack": 0,
+
     "winner": None
 }
